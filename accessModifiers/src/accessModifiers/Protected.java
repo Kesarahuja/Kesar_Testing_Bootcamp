80fe095be8b962229a91019659d5ac655001e4db
kesar <kesar.ahuja774@gmail.com>
2026-02-08 21:59:26 +0530

    access modifiers

---
+++ accessModifiers/src/accessModifiers/Protected.java
@@ -0,0 +1,11 @@
+package accessModifiers;
+
+public class Protected {
+	{
+//		Protected obj = new Protected();
+//		int output = obj.z;
+//		System.out.println("Value of z is:" + output);
+//		obj.ProtectedMethod();
+	}
+}
+}
