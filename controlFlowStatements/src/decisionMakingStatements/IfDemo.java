3db03cfbbecc9cf848838d60ec746d5b6733cf75
kesar <kesar.ahuja774@gmail.com>
2026-02-08 19:59:11 +0530

    decision statements

---
+++ controlFlowStatements/src/decisionMakingStatements/IfDemo.java
@@ -0,0 +1,12 @@
+package decisionMakingStatements;
+
+public class IfDemo {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		
+		if(x + y > 20) {
+			System.out.println("x + y is greater than 20");
+		}
+	}
+}
---
+++ controlFlowStatements/src/decisionMakingStatements/IfElseIf.java
@@ -0,0 +1,10 @@
+package decisionMakingStatements;
+
+public class IfElseIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
